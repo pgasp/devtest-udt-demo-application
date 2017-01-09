19cc241b833b121e3f313911ca94a9627b07f295
gaspa03 <gaspa03@ca.com>
2017-01-09 14:28:41 +0100

    virtualize getListUser

--- src/test/java/com/ca/devtest/lisabank/demo/sv/UserServiceTest.java
+++ src/test/java/com/ca/devtest/lisabank/demo/sv/UserServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +16,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.ca.devtest.lisabank.demo.LisaBankClientApplication;
 import com.ca.devtest.lisabank.demo.business.BankService;
 import com.ca.devtest.lisabank.wsdl.User;
+import com.ca.devtest.sv.devtools.annotation.DevTestVirtualServer;
+import com.ca.devtest.sv.devtools.annotation.DevTestVirtualService;
+import com.ca.devtest.sv.devtools.annotation.Protocol;
+import com.ca.devtest.sv.devtools.annotation.ProtocolType;
+import com.ca.devtest.sv.devtools.junit.VirtualServicesRule;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = LisaBankClientApplication.class)
-
+@DevTestVirtualServer(deployServiceToVse = "VSE")
 public class UserServiceTest {
 
 	@Autowired
 	private BankService bankServices;
 	
-
+	@Rule
+	public VirtualServicesRule rules = new VirtualServicesRule();
+	
+	@DevTestVirtualService(serviceName = "EJB3UserControlBean", port = 9081, basePath = "/itkoExamples/EJB3UserControlBean", rrpairsFolder = "UserServiceTest/getListUser/EJB3UserControlBean", requestDataProtocol = {
+			@Protocol(ProtocolType.DPH_SOAP) })
+	
 	@Test
 	public void getListUser() {
 		// Given
